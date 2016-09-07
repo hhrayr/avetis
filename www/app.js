33a088168584a7afcd95e5686e0e50b09b287825
Hrayr Harutyunyan <hrayr.harutyunyan@sixt.com>
2016-09-07 22:11:09 +0200

    code cleaning

--- www/app.js
+++ www/app.js
@@ -1,4 +1,5 @@
 import Fluxible from 'fluxible';
+
 import Application from './components/Application';
 
 import ApplicationStore from './stores/ApplicationStore';
@@ -6,12 +7,10 @@ import EnvironmentStore from './stores/EnvironmentStore';
 import NavigationStore from './stores/NavigationStore';
 import RouteStore from './stores/RouteStore';
 
-
 const app = new Fluxible({
   component: Application,
 });
 
-// register stores
 app.registerStore(ApplicationStore);
 app.registerStore(EnvironmentStore);
 app.registerStore(NavigationStore);
