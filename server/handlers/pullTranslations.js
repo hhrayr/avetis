ada210235bbd9201461273f63b56ed5db9225df6
Hrayr Harutyunyan <hrayr.harutyunyan@sixt.com>
2016-09-19 17:32:25 +0200

    refactored get translations

--- server/handlers/pullTranslations.js
+++ server/handlers/pullTranslations.js
@@ -1,11 +1,49 @@
-import { exec } from 'child_process';
+import config from '../../shared/locales/config';
+const exec = require('child_process').exec;
+import { Promise } from 'es6-promise';
+import environmentConfig from '../../www/configs/environment';
+
+class WtiPullTranslations {
+  getFileUrlForLanguage(language) {
+    const wtiUrl = `${config.wtiPath}/${config.apiKey}/files/${config.fileId}/locales/${language}`;
+    return `-O ${__dirname}/../../shared/locales/${language}.json ${wtiUrl}`;
+  }
+
+  getWgetRequestsForLanguage(language) {
+    return new Promise((resolve, reject) => {
+      exec(`wget ${this.getFileUrlForLanguage(language)}`, (error, stdout, stderr) => {
+        if (error) {
+          reject(stderr);
+        } else {
+          resolve(language);
+        }
+      });
+    });
+  }
+
+  getWgetRequestsForAllLanguages() {
+    const requests = [];
+    environmentConfig.availableLanguages.forEach((language) => {
+      requests.push(this.getWgetRequestsForLanguage(language));
+    });
+    return requests;
+  }
+
+  pullTranslationsForAllLanguages() {
+    return Promise.all(this.getWgetRequestsForAllLanguages());
+  }
+}
 
 export default (req, res) => {
-  exec('npm run wti-pull', (error, stdout, stderr) => {
-    res.status(200).send({
-      error,
-      stdout,
-      stderr,
+  const pullTranslations = new WtiPullTranslations();
+  pullTranslations.pullTranslationsForAllLanguages()
+    .then((values) => {
+      res.status(200).send({
+        status: 'OK',
+        locales: values.join(', '),
+      });
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
-  });
 };
---
+++ shared/locales/config.js
@@ -0,0 +1,7 @@
+const config = {
+  wtiPath: 'https://webtranslateit.com/api/projects',
+  apiKey: 'Fu9QTi-vuV-eU4I0PRCSmA',
+  fileId: '627294',
+};
+
+export default config;
